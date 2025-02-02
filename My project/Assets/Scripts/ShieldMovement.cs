3b099fb39f88b39c884e1310d8e4bff70b56c513
Neiiiko <113718017+Neiiiko@users.noreply.github.com>
2025-02-02 16:51:10 -0600

    Added circle and moveable square

---
+++ My project/Assets/Scripts/ShieldMovement.cs
@@ -0,0 +1,29 @@
+using System.Numerics;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class ShieldMovement : MonoBehaviour
+{
+
+    private float RadiusFromFish = 2;
+
+    private Transform tf;
+    private Vector2 mousePos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float angleRad = Mathf.Atan2(mousePos.y - 0, mousePos.x - 0);
+        tf.position = new Vector2(RadiusFromFish * Mathf.Cos(angleRad), RadiusFromFish * Mathf.Sin(angleRad));
+        tf.rotation =  Quaternion.Euler(0, 0, Mathf.Rad2Deg*angleRad + 90);
+    }
+}
